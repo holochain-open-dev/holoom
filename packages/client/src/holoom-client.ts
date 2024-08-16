020f9c49c5d49ea6655663bbe89fdf057e831d91
8e8b2c <8e8b2c@proton.me>
2024-08-16 12:01:35 +0100

    fix: also missing from rollback

--- packages/client/src/holoom-client.ts
+++ packages/client/src/holoom-client.ts
@@ -95,6 +95,7 @@ export class HoloomClient {
    */
   async setMetadata(name: string, value: string) {
     await this.usernameRegistryCoordinator.updateMetadataItem({
+      agent_pubkey: this.myPubKey,
       name,
       value,
     });
