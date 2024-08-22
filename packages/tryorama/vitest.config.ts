fb18aa1638dfbd1b6bbbb6c0f9e2c0923aef32c9
8e8b2c <8e8b2c@proton.me>
2024-08-22 08:28:18 +0100

    Revert "chore: see if single threaded tryorama un-breaks CI"
    This reverts commit 0bc9b41598e421ced634cfd27dbb1185f019ce49.

--- packages/tryorama/vitest.config.ts
+++ packages/tryorama/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    poolOptions: { threads: { singleThread: true } },
     testTimeout: 60 * 1000 * 3, // 3  mins
   },
 });
